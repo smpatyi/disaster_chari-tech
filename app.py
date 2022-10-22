ba4cd2e7957d3bf7a8b059e7ab130fb13fcc0505
Sara <sara.m.patyi@gmail.com>
2022-10-22 05:06:32 -0400

    more work on login functionality. sign up functionality needs to be created, plus further error handling and finishing the rest of the login quirks

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
 
+# Gets rid of a warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 # using flask login in order to manage the users logging in to the site
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -24,7 +27,15 @@ class User(db.Model, UserMixin):
 db.create_all()
 
 # app route for the main page that is what is seen first when app is opened
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
+def index():
+    return flask.render_template(
+        "index.html",
+    )
+
+
+# separate route because the first should just be the display, while this handles button clicks
+@app.route("/welcome", methods=["GET", "POST"])
 def welcome():
     if flask.request.method == "POST":
         # checks if the button pressed is the one for login; if yes, redirects to new HTML page
@@ -33,38 +44,49 @@ def welcome():
         # checks if the button pressed is the one for sign up; if yes, redirects to new HTML page
         elif request.form.get("sign_up") == "Sign Up":
             return flask.redirect(flask.url_for("sign_up"))
+
+
+# displays log in page with input forms
+@app.route("/log_in", methods=["GET", "POST"])
+def log_in():
+
     return flask.render_template(
-        "index.html",
+        "log_in.html",
     )
 
 
-# @app.route("/log_in", methods=["GET", "POST"])
-# def log_in():
-#     if flask.request.method == "POST":
-#         data = flask.request.form
-#         user_name = data["user_name"]
-#         password = data["password"]
+@app.route("/logged_in", methods=["GET", "POST"])
+def logged_in():
+    if flask.request.method == "POST":
+        data = flask.request.form
+        user_name = data["user_name"]
+        password = data["password"]
 
-#         # error handling for arbitrary length of inputted username and password
-#         if len(user_name) > 16:
-#             flask.flash("User name too long. Please enter a new one.")
-#             return flask.redirect(flask.url_for("log_in"))
-#         if len(password) > 20:
-#             flask.flash("Password is too long. Please enter a new one.")
-#             return flask.redirect(flask.url_for("log_in"))
+        # error handling for arbitrary length of inputted username and password
+        if len(user_name) > 16:
+            flask.flash("User name too long. Please enter a new one.")
+            return flask.redirect(flask.url_for("log_in"))
+        if len(password) > 20:
+            flask.flash("Password is too long. Please enter a new one.")
+            return flask.redirect(flask.url_for("log_in"))
 
-#         # check if it is a unique username that already exists in database
-#         if "user_name" in data:
-#             user = User.query.filter_by(user=data["user_name"]).first()
-#             if user is None:
-#                 return flask.redirect(flask.url_for("sign_up"))
-#             else:
-#                 login_user(user)
-#                 return flask.redirect(flask.url_for("index"))
-
-#     return flask.render_template(
-#         "log_in.html",
-#     )
+        # check if it is a unique username that already exists in database
+        if "user_name" in data:
+            user = User.query.filter_by(user=data["user_name"]).first()
+            if user is None:
+                flask.flash(
+                    "No account found associated with that username. Please sign up."
+                )
+                return flask.redirect(flask.url_for("sign_up"))
+            else:
+                if "password" in data:
+                    password = User.query.filter_by(password=data["password"]).first()
+                    if password is None:
+                        flask.flash("Incorrect password. Try again.")
+                        return flask.redirect(flask.url_for("log_in"))
+                    else:
+                        login_user(user)
+                        return flask.redirect(flask.url_for("main"))
 
 
 @login_manager.user_loader
